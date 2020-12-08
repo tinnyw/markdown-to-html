93076715c5a3b5674e6c15a7cf2f6850750f0004
Tinny <tinnywashington@gmail.com>
2020-12-08 15:11:31 -0500

    initial framework for replacing links

--- src/main/java/net/blueleo/markdowntohtml/MarkdownLineParser.java
+++ src/main/java/net/blueleo/markdowntohtml/MarkdownLineParser.java
@@ -5,6 +5,9 @@ import java.util.regex.Pattern;
 
 public class MarkdownLineParser {
     private static final String HEADER_MATCHER_REGEX = "^#+";
+    private static final String LINK_MATCHER_REGEX = "\\[.+\\]\\(.+\\)";
+    private static final String LINK_TEXT_MATCHER_REGEX = "\\[.+\\]";
+    private static final String LINK_URL_MATCHER_REGEX = "\\(.+\\)";
 
     /**
      * Convert a markup line to an output HTML line
@@ -29,18 +32,31 @@ public class MarkdownLineParser {
             int headerSize = headerGroup.length();
             if (headerSize > 6) headerSize = 6;
 
+            // convert markup links into HTML links, then make the whole line a header entry
             String markupLineContent = markupLine.replaceFirst(HEADER_MATCHER_REGEX, "").trim();
+            markupLineContent = convertLinkMarkupToHTML(markupLineContent);
             outputHTMLLine = String.format("<h%d>%s</h%d>", headerSize, markupLineContent, headerSize);
         } else { // make a paragraph line
+            // convert markup links into HTML links, then make the whole line a paragraph entry
+            markupLine = convertLinkMarkupToHTML(markupLine);
             outputHTMLLine = String.format("<p>%s</p>", markupLine);
         }
 
         return outputHTMLLine;
     }
 
+    private static String convertLinkMarkupToHTML(String markupLine) {
+        String markupWithLinkForHTML = markupLine;
+        Matcher patternHeaderSearch = Pattern.compile(LINK_MATCHER_REGEX).matcher(markupLine);
+        while (patternHeaderSearch.find()) {
+            String markupLinkComponent = patternHeaderSearch.group();
+        }
+        return markupWithLinkForHTML;
+    }
+
     // match the header substring in a markup line and return it (e.g. "###" and not the contents past the header)
     private static String findHeaderGroup(String markupLine) {
-        // now create the se
+        // now create the substring for the header if you can find it
         Matcher patternHeaderSearch = Pattern.compile(HEADER_MATCHER_REGEX).matcher(markupLine);
 
         if (patternHeaderSearch.find()) {
